cccb6a6aa45222804d93b579cc6b817b1a930621
Brian Sierakowski <briansierakowski@gmail.com>
2022-11-17 11:31:45 -0600

    kenny console loggins

--- bot/src/discord/commands/MegaSoundboardCommand.ts
+++ bot/src/discord/commands/MegaSoundboardCommand.ts
@@ -50,8 +50,8 @@ export class MegaSoundboardCommand extends DiscordCommand {
     }]) => {
       var soundVariantNames = soundVariants.get(sound) || [];
       console.log(`sound: ${sound}`);
-      console.log(`soundName: ${sound[name]}`);
-      console.log(`soundName2: ${sound[0]}`);
+      console.log(`soundKeys: ${Object.keys(sound)}`);
+      console.log(`soundName0: ${sound[0]}`);
       console.log(`soundVariantNames: ${soundVariantNames}`);
 
       for (let i = 0; i < soundVariantNames.length; i++) {
